e6c20ff0a56d9d3150a553aa357c34bdd6e767d9
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-05-16 14:07:13 -0400

    initial

---
+++ server.js
@@ -0,0 +1,28 @@
+var http = require("http");
+var fs = require("fs");
+
+// Set our port to 8080
+var PORT = process.env.PORT || 8080;
+
+// Create our server
+var server = http.createServer(handleRequest);
+
+// Create a function for handling the requests and responses coming into our server
+function handleRequest(req, res) {
+
+  console.log(req.url)
+
+  // Here we use the fs package to read our index.html file
+  fs.readFile(__dirname + req.url, function(err, data) {
+
+    // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
+    // an html file.
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+}
+
+// Starts our server
+server.listen(PORT, function() {
+  console.log("Server is listening on PORT: " + PORT);
+});
