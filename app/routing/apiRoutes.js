7c48fbfd91e1b6184ea3c441e57f6396c4674753
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-05-17 22:33:24 -0400

    progress

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,30 +1,11 @@
 // require friends.js
 
-app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
-
-  console.log(chosen);
-
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.json(false);
+app.get("/api/friends", function(req, res) {
+  return res.json();
 });
 
 // Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newcharacter = req.body;
-
-  console.log(newcharacter);
-
-  // We then add the json the user sent to the character array
-  characters.push(newcharacter);
-
-  // We then display the JSON to the users
-  res.json(newcharacter);
+app.post("/api/friends", function(req, res) {
+  // do some stuff with the survey results
+ 
 });
