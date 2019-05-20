5a5e6e536e5bb15d9ce031f172fdf81a67444949
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-05-19 20:29:48 -0400

    working on routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,17 @@
 // require friends.js
-const friends = require(".data/friends/js");
+const friends = require("../data/friends.js");
 
 module.exports = app => {
 
   app.get("/api/friends", (req, res) => {
-    res.json(friends);
+    res.json(friendsArray);
   });
   
 
   // Create New friends - takes in JSON input
   app.post("/api/friends", (req, res) => {
-    //if validated
+    console.log("Yay!");
+    res.json(true);
   });
   
 };
--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-const http = require("http");
-const fs = require("fs");
 const express = require("express");
-const path = require("path");
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require(".routing/apiRoutes")(app);
-require(".app/routing/htmlRoutes")(app);
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 
 app.listen(PORT, () => console.log("App listening on PORT " + PORT));
