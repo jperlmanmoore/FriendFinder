f1873150d2f801c7a247a8721c0f0e13c4b16593
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-07-10 11:56:48 -0400

    little fix

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -27,17 +27,18 @@ module.exports = app => {
     // console.log("starting search");
       for (let i = 0; i < friends.length; i++) {
         diff = 0;
-        // console.log(friends[i].name);
-        // console.log(JSON.stringify(newUser));
+        console.log(friends[i].name);
+        console.log(JSON.stringify(newUser));
         for (let j = 0; j < newUser.scores.length; j++) {
-          // console.log(newUserScore[i] + "" + friends[i].scores[j]);
+          console.log(newUserScore[i] + "" + friends[i].scores[j]);
           diff += Math.abs(parseInt(newUserScore[j]) - parseInt(friends[i].scores[j]));
           };
+          console.log(diff);
           if (diff <= friendMatch.scores && diff <= currBestMatch) {
             friendMatch.name = friends[i].name;
             friendMatch.img = friends[i].img;
             friendMatch.diff = diff;
-            // console.log(`Match found ${friendMatch.name} ${friendMatch.diff}`);
+            console.log(`Match found ${friendMatch.img} ${friendMatch.name} ${friendMatch.diff}`);
             currBestMatch = diff;
         }
       };
